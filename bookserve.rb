5abf74cd32b847d0270f21c886cfeb47bc3a0216
Nick Novitski <nicknovitski@gmail.com>
2012-02-12 22:35:47 -0800

    split up tsoy views

--- bookserve.rb
+++ bookserve.rb
@@ -3,7 +3,9 @@ require 'haml'
 
 class String
   def humanize
-    self.split("-").each{|w| w.capitalize! unless ["and", "the", "of", "a"].include? w}.join(" ")
+    readable = self.split("-").each{|w| w.capitalize! unless ["and", "the", "of", "a"].include? w}.join(" ")
+    readable[0] = readable[0].upcase
+    readable
   end
   def snake_case
     self.downcase.gsub(" ", "-").delete(",?'")
@@ -115,6 +117,27 @@ module TSoY
   def self.credit
     "Arkenstone Press 2009"
   end
+  @frontmatter = Struct::Chapter.new("Front Matter", ["Introduction", "Campaigning", "Abilities",
+                                                      "Secrets", "Keys", "Equipment"])
+  @firstmovement = Struct::Chapter.new("The Shorter Near", ["Near", "Maldor", "Sun & Moon",
+                                                            "Warcraft", "Three-Corner", "Ratkin",
+                                                            "Myth of the Skyfire"])
+  @secondmovement = Struct::Chapter.new("Matter of Ammeni", ["Ammeni", "Alchemy", "Trade",
+                                                             "Zaru", "Zu", "Khale", "Qek",
+                                                             "Knotwork", "Spiritwork",
+                                                             "The Idea of Ammeni"])
+  @thirdmovement = Struct::Chapter.new("Southern Initiative", ["Goren", "Sky God Faith",
+                                                               "Witchcraft", "Vulfen", "Vulfland",
+                                                               "Dreaming", "Second Wolf Age"])
+  @fourthmovment = Struct::Chapter.new("Species of Near", ["Human Equation", "Elves", "Goblins"])
+  @fifthmovment = Struct::Chapter.new("Margin Notes", ["Orania", "Inselburg", "Pere-di-Fey",
+                                                       "Seafaring", "Nine Celestials",
+                                                       "The Horned God", "Sireap Valley",
+                                                       "First City", "Going Out",
+                                                       "A Love Letter to a Storygamer"])
+  def self.chapters
+    [@frontmatter, @firstmovement, @secondmovement, @thirdmovement, @fourthmovment, @fifthmovment]
+  end
 end
 
 def get_module(url)
@@ -127,12 +150,6 @@ end
 
 before do
   cache_control :public, :max_age => 21600 if ENV['RACK_ENV']=="production"
-end
-
-get '/tsoy' do
-  @book = TSoY
-  @title = "World of Near"
-  haml :worldofnear
 end
 
 get '/:book/onepage' do |book|
@@ -156,6 +173,7 @@ get "/:book/:chapter" do |book, chap_name|
 
     haml :chapter
   else
+    puts "received request for unknown chapter #{@chapter} in book #{@book}"
     redirect to("/#{book}")
   end
 end
