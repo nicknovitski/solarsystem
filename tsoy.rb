cd9fb2f5cb650063930d1c11b4c1352f3d6f35c3
Nick Novitski <nicknovitski@gmail.com>
2012-02-16 13:39:10 -0800

    hide all but first chapter of tsoy

--- tsoy.rb
+++ tsoy.rb
@@ -12,59 +12,61 @@ class Tsoy < Book
     section "Equipment"
   end
 
-  chapter "The Shorter Near" do
-    section "Near"
-    section "Maldor"
-    section "Sun & Moon"
-    section "Warcraft"
-    section "Three-Corner"
-    section "Ratkin"
-    section "Myth of the Skyfire"
-  end
+  chapter "UNDER CONSTRUCTION"
 
-  chapter "Matter of Ammeni" do
-    section "Ammeni"
-    section "Alchemy"
-    section "Trade"
-    section "Zaru"
-    section "Zu"
-    section "Khale"
-    section "Qek"
-    section "Knotwork"
-    section "Spiritwork"
-    section "The Idea of Ammeni"
-  end
-
-  chapter "Southern Initiative" do
-    section "Goren"
-    section "Sky God Faith"
-    section "Witchcraft"
-    section "Vulfen"
-    section "Vulfland"
-    section "Dreaming"
-    section "Second Wolf Age"
-  end
-
-  chapter "Species of Near" do
-    section "Human Equation"
-    section "Elves"
-    section "Goblins"
-    section "Dwarves"
-    section "Giants"
-    section "Beastkin"
-    section "Nadir and Andreth"
-  end
-  chapter "Margin Notes" do
-    section "Orania"
-    section "Inselburg"
-    section "Pere-di-Fey"
-    section "Seafaring"
-    section "Nine Celestials"
-    section "The Horned God"
-    section "Sireap Valley"
-    section "First City"
-    section "Going Out"
-    section "A Love Letter to a Storygamer"
-  end
+  #chapter "The Shorter Near" do
+  #  section "Near"
+  #  section "Maldor"
+  #  section "Sun & Moon"
+  #  section "Warcraft"
+  #  section "Three-Corner"
+  #  section "Ratkin"
+  #  section "Myth of the Skyfire"
+  #end
+  #
+  #chapter "Matter of Ammeni" do
+  #  section "Ammeni"
+  #  section "Alchemy"
+  #  section "Trade"
+  #  section "Zaru"
+  #  section "Zu"
+  #  section "Khale"
+  #  section "Qek"
+  #  section "Knotwork"
+  #  section "Spiritwork"
+  #  section "The Idea of Ammeni"
+  #end
+  #
+  #chapter "Southern Initiative" do
+  #  section "Goren"
+  #  section "Sky God Faith"
+  #  section "Witchcraft"
+  #  section "Vulfen"
+  #  section "Vulfland"
+  #  section "Dreaming"
+  #  section "Second Wolf Age"
+  #end
+  #
+  #chapter "Species of Near" do
+  #  section "Human Equation"
+  #  section "Elves"
+  #  section "Goblins"
+  #  section "Dwarves"
+  #  section "Giants"
+  #  section "Beastkin"
+  #  section "Nadir and Andreth"
+  #end
+  #chapter "Margin Notes" do
+  #  section "Orania"
+  #  section "Inselburg"
+  #  section "Pere-di-Fey"
+  #  section "Seafaring"
+  #  section "Nine Celestials"
+  #  section "The Horned God"
+  #  section "Sireap Valley"
+  #  section "First City"
+  #  section "Going Out"
+  #  section "A Love Letter to a Storygamer"
+  #end
 
 end
